a26999d0b3e75e4a1e5e1eacade8d3241cab8786
“vikyyady” <vikyady1@gmail.com>
2023-06-12 14:43:05 +0800

    “Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,5 +1,13 @@
 package hitung;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika viky = new Matematika(1,0);
+        
+        System.out.println("Hasil Penjumlahan: " + viky.setPenjumlahan());
+        System.out.println("Hasil Pengurangan: " + viky.setPengurangan());
+        System.out.println("Hasil Perkalian: " + viky.setPerkalian());
+        System.out.println("Hasil Pembagian: " + viky.setPembagian());
+    }
 }
